260aee5cedf129117b3bc89534ce945814ffb3b7
Trufiadok <36418460+Trufiadok@users.noreply.github.com>
2018-11-09 19:12:59 +0100

    Repair connection to TRF_CA2_LEE_wCam part.

---
+++ IkRobotController/IkRobotController/IKRCWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IkRobotController
+{
+    class IKRCWrapper
+    {
+        private static bool isWrapped;
+
+        public interface IKRCAPI
+        {
+            bool IsInitedModule { get; }
+        }
+    }
+}
